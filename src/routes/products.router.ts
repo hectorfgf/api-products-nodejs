0ee4e3026f3dd4684213ef0b9220ed54cf24744c
hector <hector@digitalartdesigners.com>
2021-08-04 13:58:36 +0100

    Add generic error handling.

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -6,15 +6,28 @@ import AuthCheckMiddleware from "../middleware/check_auth.middleware";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const controller = new getProductController();
-    const response = await controller.getProducts(req.query.search?.toString() ??'', Number(req.query.take)?? null, Number(req.query.skip)?? null);
-    return res.send(response);
+    try{
+        const controller = new getProductController();
+        const response = await controller.getProducts(req.query.search?.toString() ??'', Number(req.query.take)?? null, Number(req.query.skip)?? null);
+        return res.send(response);
+    }catch (e){
+        return res.status(500).send({
+            message: 'Error!'
+        });
+    }
+
 });
 
 router.post("/", AuthCheckMiddleware.validateToken, async (req, res) => {
-    const controller = new createProductsController();
-    const response = await controller.createProduct(req.body);
-    return res.send(response);
+    try {
+        const controller = new createProductsController();
+        const response = await controller.createProduct(req.body);
+        return res.send(response);
+    }catch (e){
+        return res.status(500).send({
+            message: 'Error!'
+        });
+    }
 });
 
 export default router;
